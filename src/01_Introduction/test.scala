f42938fe55a3d4b20d1bc425ffe9832aa6830b1b
dennis.madsen <dennis.madsen@unibas.ch>
2024-02-09 21:15:10 +0100

    misc: update to use newest scalismo

--- src/01_Introduction/test.scala
+++ src/01_Introduction/test.scala
@@ -1,9 +1,9 @@
 //> using scala "3.3"
-//> using dep "ch.unibas.cs.gravis::scalismo-ui:0.92.0,exclude=ch.unibas.cs.gravis%vtkjavanativesmacosimpl"
+//> using dep "ch.unibas.cs.gravis::scalismo-ui:1.0.0"
 
 import scalismo.ui.api.ScalismoUI
 
 object HelloScalismo extends App {
-    scalismo.initialize()
+    println("Hello Scalismo!")
     ScalismoUI()
 }
