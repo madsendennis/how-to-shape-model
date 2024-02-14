ac5bc1cea69f15aa2801adc1d8f501f771af53ac
dennis.madsen <dennis.madsen@unibas.ch>
2024-02-14 09:08:31 +0100

    feat: create vertebrea gpmm model

---
+++ src/prepare_data/02_defineModel.scala
@@ -0,0 +1,59 @@
+//> using scala "3.3"
+//> using dep "ch.unibas.cs.gravis::scalismo-ui:1.0.0"
+
+import scalismo.ui.api.ScalismoUI
+import scalismo.io.MeshIO
+import java.io.File
+import scalismo.io.LandmarkIO
+import scalismo.geometry._3D
+import scalismo.registration.LandmarkRegistration
+import scalismo.geometry.Point3D
+import scalismo.statisticalmodel.PointDistributionModel3D
+import scalismo.kernels.GaussianKernel
+import scalismo.kernels.DiagonalKernel
+import scalismo.mesh.TriangleMesh
+import scalismo.statisticalmodel.LowRankGaussianProcess
+import scalismo.common.interpolation.TriangleMeshInterpolator3D
+import scalismo.statisticalmodel.GaussianProcess3D
+import scalismo.geometry.EuclideanVector3D
+import scalismo.common.Field
+import scalismo.common.EuclideanSpace3D
+import scalismo.geometry.Point
+import scalismo.geometry.EuclideanVector
+import scalismo.common.interpolation.NearestNeighborInterpolator3D
+import scalismo.io.StatisticalModelIO
+import scalismo.statisticalmodel.PointDistributionModel
+
+def createModel(ref: TriangleMesh[_3D]): PointDistributionModel[_3D, TriangleMesh] =
+    val scalingCoarse = 50.0
+    val sigmaCoarse = 35.0
+    val scalingFine = scalingCoarse/4.0
+    val sigmaFine = sigmaCoarse/4.0
+    val relativeTolerance = 0.01
+
+    val kernelCoarse = GaussianKernel[_3D](sigmaCoarse) * scalingCoarse
+    val kernelFine = GaussianKernel[_3D](sigmaFine) * scalingFine
+    val kernel = kernelCoarse + kernelFine
+    val gp = GaussianProcess3D[EuclideanVector[_3D]](DiagonalKernel(kernel, 3))
+    val lrGP = LowRankGaussianProcess.approximateGPCholesky(ref, gp, relativeTolerance, interpolator = NearestNeighborInterpolator3D())
+    val gpmm = PointDistributionModel3D(ref, lrGP)
+    gpmm
+
+@main def createGPMM() = 
+    println("Create deformable model for non-rigid registration")
+    val dataFolder = new File("data/vertebrae/")
+    val ref = MeshIO.readMesh(new File(dataFolder, "ref_20.ply")).get
+    val gpmmFile = new File(dataFolder, "gpmm.h5.json")
+
+    val gpmm = StatisticalModelIO.readStatisticalTriangleMeshModel3D(gpmmFile).getOrElse({
+        println("Model not found, creating new one")
+        val tmp = createModel(ref)
+        StatisticalModelIO.writeStatisticalTriangleMeshModel3D(tmp, gpmmFile)
+        tmp
+    })
+
+    println(s"Model rank: ${gpmm.rank}")
+
+    val ui = ScalismoUI()
+    ui.show(gpmm, "gpmm")
+
